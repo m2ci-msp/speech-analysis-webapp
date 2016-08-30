1f60286a2b5fead090ea109bc90612204b82b4a7
Ingmar Steiner <steiner@coli.uni-saarland.de>
2016-08-30 16:40:33 +0200

    toward using magic annotations from spring boot test

--- src/test/java/com/speechanalysis/selenium/LoginTest.java
+++ src/test/java/com/speechanalysis/selenium/LoginTest.java
@@ -1,14 +1,20 @@
 package com.speechanalysis.selenium;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 /**
  * Created by plam on 25/08/16.
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class LoginTest {
     private int port = 8080;
     @Test
